a8cbc5c9ef37180bfd4bd33644eb02fac0df45b3
Judith Koelewijn <koelewijnjudith@hotmail.com>
2021-06-08 23:53:11 +0200

    succesfully updated user location with mongodb

--- matching-app.js
+++ matching-app.js
@@ -9,6 +9,14 @@ import User from "./models/users.js";
 
 
 
+app.engine('handlebars', expbs({
+    defaultLayout: 'main',
+}));
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 
 
 // connect to MongoDB via Mongo instructies//
@@ -20,11 +28,8 @@ mongoose.connect(dbURI, {
     .then((result) => app.listen(3000), console.log('connected'))
     .catch((err) => console.log('error'));
 
-import bodyParser2 from 'body-parser';
-import bodyParser from 'body-parser';
-import {
-    name
-} from 'ejs';
+
+
 
 //localhost verbinding of verbinding via Heroku //
 app.listen(process.env.PORT);
@@ -33,23 +38,6 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-
-//Mongoose Routes testing // 
-
-app.get('/add-user', (req, res) => {
-    const user = new User({
-        name: 'New User',
-        location: 'Amsterdam West',
-        interests: 'Voetbal'
-    });
-    user.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-})
 
 
 
@@ -75,38 +63,10 @@ app.post('/results', (req, res) => {
 
 
 
-
-
-//find matching paramater testing op console.log//
-//https://thecodebarbarian.com/how-find-works-in-mongoose.html//
-
-User.findOne().sort({
-    _id: -1
-}).exec((err, docs) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("The latest added new user:", docs);
-        return;
-    }
-});
-
-
-
-
 // pagina met matchende gebruikers//
-// async function om match te vinden en naar /results te sturen (bron: uitgewerkt met Marvin) //
+// async function om match te vinden en naar /results te sturen (uitgewerkt met Marvin) //
 
-const allUsers = await User.find({});
-console.log(allUsers);
-const lastUser = await User.findOne().sort({
-    _id: -1
-});
 
-const matches = await User.find({
-    location: lastUser.location
-})
-console.log('matches console log:', matches);
 
 app.get('/results', async (req, res) => {
     const allUsers = await User.find({});
@@ -116,132 +76,163 @@ app.get('/results', async (req, res) => {
     const matches = await User.find({
         location: lastUser.location
     })
+    console.log(matches);
     res.render('results', {
-        user: matches
-    });
+        users: matches
+
+    })
 
 
 })
 
 
+app.get('/profile', async (req, res) => {
+    const lastUser = await User.findOne().sort({
+        _id: -1
+    });
+    console.log(lastUser);
+    res.render('profile', {
+        laatsteGebruiker: lastUser
+
+    })
+})
+
+
+/* new findone and update test */
 
 
 
 
 
+
+app.post('/profile-changed', async (req, res) => {
+    const lastUser = await User.findOne().sort({
+        _id: -1
+    });
+    const updateUser = await User.findOneAndUpdate({
+        name: lastUser.name
+    }, {
+        location: 'Rotterdam'
+    }, {
+        new: true
+    }
+    );
+    res.render('profile-changed', {
+        updatedUser: updateUser
+        
+
+    })
+    
+    
+    
+})
+
+
+const lastUser = await User.findOne().sort({
+    _id: -1
+});
+
+
+
+User.findOneAndUpdate({name: lastUser.name}, {location: 'Rotterdam'}, { new: true},function(err, doc){
+    if(err){
+        console.log("Something wrong when updating data!");
+    }
+
+    console.log('consoletest"',doc);
+});
+
+
+
+
+
+
+
+
+
+
+const updateUser = await User.findOneAndUpdate({
+    name: lastUser.name
+}, {
+    location: 'Rotterdam'
+}, {
+    new: true
+}
+);
+
+
+
+
+/* results when location has changed */
+
+app.post('/updated-matches', async (req, res) => {
+    const sendUser = await User.find({
+        location: updateUser.location
+    })
+    res.render('updated-matches', {
+        nieuweMatches: sendUser
+    })
+});
+
+
+
+
+
+
+
+
+
+
+//profile changed location//
+/*
+const lastUser = await User.findOne().sort({
+    _id: -1
+});
+
+
+//Werkende code om 1 paramater te wijzigen en user succesvol te updaten//
 // Het updaten van de locatie van een gebruiker //
 //https://stackoverflow.com/questions/7267102/how-do-i-update-upsert-a-document-in-mongoose//
 
 
-User.findOneAndUpdate({
-    name: 'Yara Joseph'
+
+
+const updateUser = await User.findOneAndUpdate({
+    name: lastUser.name
 }, {
-    location: 'Amstelveen'
+    location: 'Rotterdam'
 }, {
     new: true
-}, (error, data) => {
-    if (error) {
-        console.log(error)
-    } else {
-        console.log(data)
-    }
+}
+);
+
+
+/* creating a post method route */
+
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile')
+});
+
+app.get('/profile-changed', (req,res) => {
+    res.render('profile-changed')
+});
+
+app.get('/updated-matches', (req, res) => {
+    res.render('updated-matches')
 })
 
 
-
-
-
-//setting handlebars en defaultLayout//
-
-
-app.engine('handlebars', expbs({
-    defaultLayout: 'main',
-}));
-app.set('view engine', 'handlebars');
-
-// tests voor maken routes // 
-
-app.get('/', function (req, res) {
-        res.render('index', {
-            title: 'Welcome Judith'
-        });
-
-
-        var urlencodedParser = bodyParser.urlencoded({
-            extended: false
-        });
-
-
-        app.post('/about', urlencodedParser, function (req, res) {
-            console.log(req.body);
-            res.render('about-succes', {
-                data: req.body
-            });
-        });
-
-
-
-
-
-
-
-
-
-        //routes and 404//
-
-
-        app.get('/', (req, res) => {
-            res.render('index');
-        });
-
-        app.get('/match-1', (req, res) => {
-            res.render('match-1');
-        })
-
-        app.get('/match-2', (req, res) => {
-            res.render('match-2');
-        })
-        app.get('/match-3', (req, res) => {
-            res.render('match-3')
-        })
-        app.get('/home', (req, res) => {
-            res.render('home');
-        });
-
-        app.get('/match-succes', (req, res) => {
-            res.render('match-succes');
-        })
-
-        app.get('/results', (req, res) => {
-            res.render('results');
-        })
-
-
-        app.get('/about', (req, res) => {
-            res.render('about', {
-                title: 'Test about title',
-                name: 'Judith Koelewijn'
-            });
-        })
-
-
-        // Custom 404 melding //
-
-        app.use(function (req, res, next) {
-
-            res.status(404).send("404: Sorry, the page you're looking for does not exist");
-
-        });
-
-        app.get('/home', (req, res) => {
-            res.render('home');
-        })
-    }
-
-
-)
-
-/* Verbinding maken met statische files zoals images en css */
 
 
 
